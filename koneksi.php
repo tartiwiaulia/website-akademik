4f5c8e9b75aded0ff3f10f94c86bb088eaec4850
tartiwiaulia <tartiwiap@gmail.com>
2026-01-14 23:47:16 +0700

    update 14 01 2026

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,11 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "db_akademik");
+$host = "127.0.0.1";
+$user = "root";
+$pass = "";
+$db   = "db_akademik";
+$port = 3306;
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
